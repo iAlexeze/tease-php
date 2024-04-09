8a2c87f9a3ace6e166483207b8152bf0b11ae681
ialexeze <ialexeze@gmail.com>
2024-04-09 15:56:28 +0100

    today

--- products.php
+++ products.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Available Products</title>
+    <title>Available Products Today</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
